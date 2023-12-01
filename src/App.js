500cc1e6dc2240bec133fcba1b0d74342178b0c0
Austin Doty <asrdoty@gmail.com>
2023-12-01 15:31:45 -0500

    checkout page, with cart functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
 
-import { UserContext } from "./context/user-context";
+import { Routes, Route } from "react-router-dom";
+
+
 
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 const App = () => {
-  const { currentUser } = useContext(UserContext);
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route
-          path="auth"
-          element={
-            currentUser ? <Navigate to="/" replace /> : <Authentication />
-          }
-        />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
