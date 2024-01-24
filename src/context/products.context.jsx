e84454517ab8869828df4b896f576e204561febf
Austin Doty <asrdoty@gmail.com>
2024-01-24 14:22:30 -0500

    Calling in FireStore Data Category Map

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 
-import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
-
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
@@ -10,7 +9,11 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    addCollectionAndDocuments("categories");
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
   }, []);
   const value = { products };
   return (
