f9f5cd8734a84b377800d5fda0e7812e596b3853
Austin Doty <asrdoty@gmail.com>
2024-01-25 14:14:09 -0500

    SCCS Debugging and Category Pages Finished

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import ProductCard from "../product-card/product-card.component";
 
 import "./category-preview.styles.scss";
@@ -5,6 +7,10 @@ import "./category-preview.styles.scss";
 const CategoryPreview = ({ title, products }) => (
   <div className="category-preview-container">
     <h2>
+      <Link className="title" to={title}>
+        {" "}
+        {title.toUpperCase()}{" "}
+      </Link>
       <span className="title">{title.toUpperCase()}</span>
     </h2>
     <div className="preview">
