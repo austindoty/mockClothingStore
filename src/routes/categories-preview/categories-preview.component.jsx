7a2bee4255af175a93cb66701c98c35010892cbf
Austin Doty <asrdoty@gmail.com>
2024-01-25 13:45:47 -0500

    Categories Routes Implemented

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,17 +1,18 @@
-import { Fragment, useContext } from "react";
-
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { useContext, Fragment } from "react";
 
 import { CategoriesContext } from "../../context/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((key) => {
-        const products = categoriesMap[key];
-        return <CategoryPreview key={key} title={key} products={products} />;
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
       })}
     </Fragment>
   );
